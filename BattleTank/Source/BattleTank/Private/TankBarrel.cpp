ee6f0ccccc04f861ef3036855fdd0349f7d9370c
Nathan Newman <nathan.en@gmail.com>
2017-04-10 21:38:08 -0400

    Using FMath::Clamp

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -8,5 +8,12 @@ void UTankBarrel::MoveTo(float Pitch) {
 
 	// move the barrel the right amount this frame
 	// given a max elevation speed, and the frame time
+
+	// auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	Pitch = FMath::Clamp<float>(Pitch, -1, 1);
+	auto ElevationChange = Pitch * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+		SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
