e110f00352717c2c0755d4e7b0f0ef54e837f156
Nathan Newman <nathan.en@gmail.com>
2017-04-14 23:52:03 -0400

    Projectile Beginnings

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -31,8 +31,13 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+}
+
+void ATank::Fire()
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Tank Fires"), Time);
 }
 
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
