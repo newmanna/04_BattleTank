e7288e60b7bbec46012418d2751fee9693149999
Nathan Newman <nathan.en@gmail.com>
2017-05-23 16:05:19 -0400

    Using UENUM()

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,6 +5,16 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for firing state
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
+// Forward Declarations
 class UTankBarrel;
 class UTankTurret;
 
@@ -23,6 +33,10 @@ public:
 	// TODO add set turret reference
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
 
 private:
 	UTankBarrel* Barrel = nullptr;	
